feff217fd881576a9e3e9bcf6c3f87fc3f6ca4dc
ccdode <calculusdode@gmail.com>
2026-07-30 02:07:23 +0100

    function with parameters and argument for vessel reports

---
+++ 04-Functions/04-02-02-vessel-report.js
@@ -0,0 +1,21 @@
+  function createVoyageReport(vessel, port, cargo){
+
+    console.log("=== VOYAGE REPORT ===");
+
+    console.log("Vessel:", vessel);
+
+    console.log("Port:", port);
+
+    console.log("Cargo:", cargo);
+
+}
+
+createVoyageReport(
+
+    "MT Atlantic",
+
+    "Tema",
+
+    "Crude Oil"
+
+);
