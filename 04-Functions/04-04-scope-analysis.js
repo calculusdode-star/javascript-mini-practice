84ad99ec21c0ed93052b32e9c2048b3975b84bc7
ccdode <calculusdode@gmail.com>
2026-07-30 02:55:05 +0100

    lexical scope

---
+++ 04-Functions/04-04-scope-analysis.js
@@ -0,0 +1,27 @@
+const company = "Ocean Shipping";
+
+
+function operation(){
+
+    const vessel = "MT Atlantic";
+
+
+    function report(){
+
+        const cargo = "Crude Oil";
+
+        console.log(company);
+
+        console.log(vessel);
+
+        console.log(cargo);
+
+    }
+
+
+    report();
+
+}
+
+
+operation();
